188fed3c5a7f4d4947776aeae435ab646c92df80
JuanDiegoTP <73618453+JuanDiegoTerriquezPolanco@users.noreply.github.com>
2022-02-27 15:59:38 -0600

    PRACTICA_4-objects

--- modules/app.js
+++ modules/app.js
@@ -1,2 +1,21 @@
 let greet = require('./greet');
 greet();
+
+let persona = {
+    nombre: "Carlos",
+    apellidos: "Ulibarri",
+    getName: function(){
+        return `Nombre: ${this.nombre}, Apellido: ${this.apellidos}`
+    },
+    domicilio: {
+        calle: "Madero #144",
+        colonia: "Centro",
+        cp: 28000,
+        municipio: "Colima"
+    },
+    getAdress: function(){
+        return `Calle: ${this.domicilio.calle}, Colonia: ${this.domicilio.colonia}, CP: ${this.domicilio.cp}, Municipio: ${this.domicilio.municipio}`
+    }
+}
+console.log(persona.getName());
+console.log(persona.getAdress());

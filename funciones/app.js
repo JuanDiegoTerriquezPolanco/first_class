d229125fb22ce918bc87e76c824878de83022673
JuanDiegoTP <73618453+JuanDiegoTerriquezPolanco@users.noreply.github.com>
2022-02-27 14:11:01 -0600

    refactor ES6

--- funciones/app.js
+++ funciones/app.js
@@ -1,20 +1,14 @@
-function greet(){
-    console.log("Hello");
-}
+let greet = () => console.log("Hello");
 greet(); //inveke
 
 //functions as parameters
-function logGreeting(fn){
-    fn();
-};
+let logGreeting = fn => fn();
 logGreeting(greet);
 //estamos pasando la función greet como
 //parámetro e invocandola con el nombre del argumento fn
 
 //funtion expression
-let greetMe = function(){
-    console.log("Hello from the function expression");
-}
+let greetMe = () => console.log("Hello from the function expression");
 greetMe();
 
 logGreeting(greetMe);

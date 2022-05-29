bc7cb009f4079c678d4573a870450079ee437a07
JuanDiegoTP <73618453+JuanDiegoTerriquezPolanco@users.noreply.github.com>
2022-05-28 20:45:27 -0500

    PRACTICA_21

--- practica_19/routes/persons.js
+++ practica_19/routes/persons.js
@@ -3,25 +3,30 @@ const router = express.Router();
 const mongoose = require('../node_modules/mongoose');
 let Person = require('../models/person');
 
-    router.get('/persons', function(req,res,next) {
-        Person.find(function(err,person){
-            if(err) return next(err);
-            res.json(person);
-        });
+router.get('/persons', function(req, res, next) {
+    Person.find(function(err, persons) {
+        if (err) return next(err);
+        //res.json(person);
+        res.render('personsIndex', { persons });
     });
+});
 
-    router.get('/person', function(req, res){
-        res.render('person');
-    });
+router.get('/person', function(req, res) {
+    res.render('person');
+});
+router.get('/main', function(req, res) {
+    res.render('main');
+});
 
-    router.post('/addPerson', function(req, res){
-        // console.log(req.body);
-        const myPerson = new Person({
-            nombre: req.body.nombre,
-            edad: req.body.edad,
-            tipoSangre: req.body.tipoSangre,
-            nss: req.body.nss});// crea la entidad
-        myPerson.save();//guarda en bd
-    });
-    
-module.exports=router;
+router.post('/addPerson', function(req, res) {
+    // console.log(req.body);
+    const myPerson = new Person({
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        tipoSangre: req.body.tipoSangre,
+        nss: req.body.nss
+    }); // crea la entidad
+    myPerson.save(); //guarda en bd
+});
+
+module.exports = router;
